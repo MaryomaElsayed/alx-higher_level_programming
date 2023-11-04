135fe8b316608f468c7df0ec394ad586afb3a0ba
MaryomaElsayed <maryamelsayd.1@gmail.com>
2023-11-04 06:08:11 -0700

    hi

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Defines an inherited list class MyList."""
 class Myclass(list):
     def __init__(self):
         super().__init__()
 
     def print_sorted(self):
+                """Print a list in sorted ascending order."""
         print(sorted(self))
